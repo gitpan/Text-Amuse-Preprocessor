d53935964b360d84ca23a0c3576faec9c3b28e37
Marco Pessotto <melmothx@cpan.org>
2014-01-26 09:24:53 +0000

    Import of MELMOTHX/Text-Amuse-Preprocessor-0.01 from CPAN.
    gitpan-cpan-distribution: Text-Amuse-Preprocessor
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MELMOTHX/Text-Amuse-Preprocessor-0.01.tar.gz
    gitpan-cpan-author:       MELMOTHX
    gitpan-cpan-maturity:     released

---
+++ bin/muse-rearrange-footnotes.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Pod::Usage;
+
+=head1 NAME
+
+muse-rearrange-footnotes.pl
+
+=head1 DESCRIPTION
+
+This script takes a file as argument, and rearrange the footnotes
+numbering, barfing if the footnotes found in the body don't match the
+footnotes themselves. This is handy if you inserted footnotes at
+random position, or if the footnotes are numbered by section or
+chapter.
+
+Example input file content:
+
+  This [1] is a text [1] with three footnotes [4]
+
+  [1] first
+  
+  [1] second
+  
+  [2] third
+
+
+Output in file with C<.fixed> extension:
+
+  This [1] is a text [2] with three footnotes [3]
+
+  [1] first
+  
+  [2] second
+  
+  [3] third
+  
+The original file is never overwritten.
+
+=head1 SYNOPSIS
+
+  muse-rearrange-footnotes.pl file.muse
+
+=head1 SEE ALSO
+
+L<Text::Amuse::Preprocessing>
+
+=cut
+
+my $filename = shift;
+
+die pod2usage("Please pass a file.muse as argument\n") unless $filename;
+
+die pod2usage("$filename doesn't exist\n")      unless -e $filename;
+die pod2usage("$filename is not a text file\n") unless -T $filename;
+
+my $outputfile = $filename . ".fixed" ; 
+print "Processing $filename, I'll output on $outputfile\n Please double check the result\n";
+
+open(my $in, "<", $filename) or die "Cannot open $filename, $!\n";
+
+# read the file.
+my $fn_counter = 0; 
+my $body_fn_counter = 0;
+my $last_was_fn = 0;
+my @fnotes;
+my @orig_body;
+
+while (my $r = <$in>) {
+	if ($r =~ m/^\s*\[\d+\]\s*/) {
+		$r =~ s/^\s*\[\d+\]/"[" . ++$fn_counter . "]"/e;
+		# the footnotes at the end go in a separate array
+		push @fnotes, $r;
+        $last_was_fn = 1;
+        next;
+	}
+
+    # check if we have a broken footnote and skip the first empty line
+    # after that.
+    if ($last_was_fn) {
+        if ($r =~ m/^\s*$/) {
+            $last_was_fn = 0;
+            next;
+        }
+        else {
+            die "Broken footnote detected: " . $fnotes[$#fnotes];
+        }
+    }
+
+    # then process the page
+    $r =~ s/\[\d{1,4}\]/"[" . ++$body_fn_counter . "]"/ge;
+    push @orig_body, $r; 
+}
+
+close $in;
+
+if ($body_fn_counter != $fn_counter) {
+    warn "Counter mismatch: body has $body_fn_counter reference, " 
+      . "but $fn_counter footnotes found\n";
+    $outputfile .= ".broken.muse";
+    warn "Output on $outputfile\n";
+}
+
+
+# write the body file
+open(my $out, ">", $outputfile) or die "I cannot open $outputfile, $!";
+
+while (@orig_body) {
+	print $out shift(@orig_body);
+}
+while (@fnotes) {
+	print $out shift(@fnotes), "\n";
+}
+close $out;
+
---
+++ lib/Text/Amuse/Preprocessor.pm
@@ -0,0 +1,70 @@
+package Text::Amuse::Preprocessor;
+
+use 5.010001;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Text::Amuse::Preprocessor - Helpers for Text::Amuse document formatting.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+This module by itself doesn't do anything, but the bundled modules do.
+
+  use Text::Amuse::Preprocessor::HTML qw/html_to_muse/;
+  my $html = '<p>Your text here... &amp; &quot; &ograve;</p>'
+  my $muse = html_to_muse($html);
+
+  use Text::Amuse::Preprocessor::Typography qw/typography_filter
+                                               linkify_filter/;
+  $muse = typography_filter(en => $muse);
+  $muse = linkify_filter($muse);
+
+=head1 AUTHOR
+
+Marco Pessotto, C<< <melmothx at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the author's email. If
+you find a bug, please provide a minimal muse file which reproduces
+the problem (so I can add it to the test suite).
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Text::Amuse::Preprocessor
+
+Repository available at Gitorious:
+L<https://gitorious.org/text-amuse-preprocessor>
+
+=head1 SEE ALSO
+
+The original documentation for the Emacs Muse markup can be found at:
+L<http://mwolson.org/static/doc/muse/Markup-Rules.html>
+
+The parser itself is L<Text::Amuse>.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Text::Amuse::Preprocessor
