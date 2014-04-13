f102caa84f28c392205545dc94100b29fd50e17c
Marco Pessotto <melmothx@cpan.org>
2014-04-13 17:52:58 +0000

    Import of MELMOTHX/Text-Amuse-Preprocessor-0.06 from CPAN.
    gitpan-cpan-distribution: Text-Amuse-Preprocessor
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MELMOTHX/Text-Amuse-Preprocessor-0.06.tar.gz
    gitpan-cpan-author:       MELMOTHX
    gitpan-cpan-maturity:     released

--- lib/Text/Amuse/Preprocessor.pm
+++ lib/Text/Amuse/Preprocessor.pm
@@ -10,11 +10,11 @@ Text::Amuse::Preprocessor - Helpers for Text::Amuse document formatting.
 
 =head1 VERSION
 
-Version 0.04
+Version 0.06
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
